1bf273ebebf6b7c1c3ac4eccea351213ecf63264
trhanhtu <tr.ho.anh.tu@gmail.com>
2024-07-29 17:27:25 +0700

    page

---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-DYRWTJPD.mjs";import"./chunk-ISTX47JU.mjs";import"./chunk-RH2KI2GY.mjs";import"./chunk-TLMKFP4B.mjs";import"./chunk-34T5BS2P.mjs";import"./chunk-I5JFDTHP.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- src/app/main/routes/main.routes.ts
+++
@@ -1,5 +0,0 @@
-import { Routes } from "@angular/router";
-
-export const mainRoutes: Routes[] = [
-
-];
